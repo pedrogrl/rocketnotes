fda7a6db5adaecc0c3984789ee682523f40ca373
pedrogrl <pedro.gos@outlook.com>
2022-12-28 20:16:31 -0800

    added app/auth routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <AppRoutes /> {/* logged in */}
+      {/* <AuthRoutes /> */} {/* logged out */}
+    </BrowserRouter>
+  );
+}
