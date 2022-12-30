f94f8830f143e6bb0138e0af7d6fa54aaef94a18
pedrogrl <pedro.gos@outlook.com>
2022-12-29 22:21:37 -0800

    handleSignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,24 @@
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-import {FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock } from "react-icons/fi";
 import { Background, Container, Form } from "./styles";
 
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
+import { useAuth } from "../../hooks/auth";
+
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
@@ -19,21 +31,21 @@ export default function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={(ev) => setEmail(ev.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={(ev) => setPassword(ev.target.value)}
         />
 
-        <Button title="Entrar"/>
+        <Button title="Entrar" onClick={handleSignIn} />
 
-        <Link to="/register">
-          Criar conta
-        </Link>
+        <Link to="/register">Criar conta</Link>
       </Form>
-      <Background/>
+      <Background />
     </Container>
-  )
+  );
 }
